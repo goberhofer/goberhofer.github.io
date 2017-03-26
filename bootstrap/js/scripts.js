3613528c512e62078f07ed70e0dd608162dfa5d5
shalarn1 <shalarn1@jhu.edu>
2017-03-26 12:32:13 -0400

    add all pages except music-tunes, contact non-functional

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,4 +1,4 @@
-var links = document.getElementById('navbar-collapse-1').getElementsByTagName('a');
+var links = document.getElementById('navs').getElementsByTagName('a');
 
 for(var i = 0, link; link = links[i]; i++) {
         link.onclick = showContent;
@@ -9,36 +9,54 @@ for(var i = 0, link; link = links[i]; i++) {
         	continue;
         }
         contentdiv.style.display = 'none';
+}
+
+// Show the first content div
+if(links.length > 0) showContent.apply(links[0]);
+var current;
+
+function showContent() {
+
+    // hide old content
+    if(current) current.style.display = 'none';
+
+    current = getContentDiv(this);
+    if(!current) return true;
+
+    //current.innerHTML = "This is where the xml variable content should go";
+    current.style.display = 'inline-block';
+
+    return true;
+
+}
+
+function getContentDiv(link) {
+
+    var linkTo = link.getAttribute('href');
+    console.log(linkTo);
+
+    // Make sure the link is meant to go to a div
+    if(linkTo.substring(0, 2) != '#!') return;
+    linkTo = linkTo.substring(2);
+	console.log(linkTo);
+    return document.getElementById(linkTo);
+
+}
+
+function toggle_display(a, b){
+    var x = a
+    var y = b
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
     }
 
-    // Show the first content div
-   if(links.length > 0) showContent.apply(links[0]);
-   var current;
-
-   function showContent() {
-
-        // hide old content
-        if(current) current.style.display = 'none';
-
-        current = getContentDiv(this);
-        if(!current) return true;
-
-        //current.innerHTML = "This is where the xml variable content should go";
-        current.style.display = 'inline-block';
-
-        return true;
-
+    if (y.style.display === 'none') {
+        y.style.display = 'flex';
+    } else {
+        y.style.display = 'none';
     }
-
-    function getContentDiv(link) {
-
-        var linkTo = link.getAttribute('href');
-        console.log(linkTo);
-
-        // Make sure the link is meant to go to a div
-        if(linkTo.substring(0, 2) != '#!') return;
-        linkTo = linkTo.substring(2);
-		console.log(linkTo);
-        return document.getElementById(linkTo);
-
-    }
+}
+// var logolink = document.getElementById('logo-pic');
+// logolink.onclick = showContent.apply(logolink)
